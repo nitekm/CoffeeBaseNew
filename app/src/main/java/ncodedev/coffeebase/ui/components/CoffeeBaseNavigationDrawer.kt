136fb7810ca61b08d467970bfe79bd051a2bed64
ncode <developerncode@gmail.com>
2024-02-18 12:13:34 +0100

    MyCoffeeBaseScreen with TopAppBar and Navigation Drawer

--- app/src/main/java/ncodedev/coffeebase/ui/components/CoffeeBaseNavigationDrawer.kt
+++ app/src/main/java/ncodedev/coffeebase/ui/components/CoffeeBaseNavigationDrawer.kt
@@ -1,12 +1,19 @@
 package ncodedev.coffeebase.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import ncodedev.coffeebase.R
@@ -73,5 +80,16 @@ fun getNavigationDrawerItems(): List<NavigationItem> {
             Icons.Filled.Lock
         ),
     )
+}
+
+@Composable
+private fun NavDrawerHeader() {
+    Row(modifier = Modifier.fillMaxWidth()
+        .padding(top = 20.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+
+    }
 }
 
