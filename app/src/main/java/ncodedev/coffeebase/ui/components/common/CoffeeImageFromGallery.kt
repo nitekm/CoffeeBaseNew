fe32f1c59bebdc75023c3ab9d6e6d4f19cd1bfa4
ncode <developerncode@gmail.com>
2024-03-09 22:22:31 +0100

    CB-221 Edit Coffee

--- app/src/main/java/ncodedev/coffeebase/ui/components/common/CoffeeImageFromGallery.kt
+++ app/src/main/java/ncodedev/coffeebase/ui/components/common/CoffeeImageFromGallery.kt
@@ -14,11 +14,12 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -35,6 +36,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import coil.compose.rememberAsyncImagePainter
 import ncodedev.coffeebase.R
 import ncodedev.coffeebase.ui.screens.editcoffee.PermissionsViewModel
+import ncodedev.coffeebase.ui.utils.ReadMediaPermissionResultLauncher
 
 @Composable
 fun CoffeeImageFromGallery(modifier: Modifier = Modifier) {
@@ -44,6 +46,10 @@ fun CoffeeImageFromGallery(modifier: Modifier = Modifier) {
     val imageBitMap: MutableState<Bitmap?> = remember { mutableStateOf(null) }
     val showDialog = remember { mutableStateOf(false) }
 
+    //TODO: przesunac launchery do innego pliku, następnie po wybraniu opcji z dialogu czy camera czy gallery odpalic odpowiedni
+    // request o permissiony. Permissiony on isGranted wolaja view model z parametrem ktiory permisison zostal udzielony,
+    //  view model zmiania stateOfBooleany od odpowiedniej opcji (camera, gallery) i odpala launcher do wybory obrazka camera lub gallery
+    val xyz = ReadMediaPermissionResultLauncher(permissionsViewModel = permissionsViewModel)
     val readMediaPermissionResultLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestPermission(),
         onResult = { isGranted ->
@@ -52,10 +58,12 @@ fun CoffeeImageFromGallery(modifier: Modifier = Modifier) {
                 isGranted = isGranted
             )
             if (isGranted) {
+
                 showDialog.value = true
             }
         }
     )
+
 
     val readWriteStoragePermissionResultLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.RequestMultiplePermissions(),
@@ -89,7 +97,21 @@ fun CoffeeImageFromGallery(modifier: Modifier = Modifier) {
 
     ImageChoiceDialog(showDialog) { choice ->
         when (choice) {
-            0 -> cameraLauncher.launch(null)
+            0 -> {
+                permissionsViewModel.dismissDialog()
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    readMediaPermissionResultLauncher.launch(
+                        Manifest.permission.READ_MEDIA_IMAGES
+                    )
+                } else {
+                    readWriteStoragePermissionResultLauncher.launch(
+                        listOf(
+                            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                            Manifest.permission.READ_EXTERNAL_STORAGE
+                        ).toTypedArray()
+                    )
+                }
+            }
             1 -> galleryLauncher.launch("image/*")
         }
     }
@@ -161,13 +183,29 @@ fun ImageChoiceDialog(showDialog: MutableState<Boolean>, onOptionSelected: (Int)
             confirmButton = { },
             dismissButton = { },
             text = {
-                Column {
-                    TextButton(onClick = { onOptionSelected(0); showDialog.value = false }) {
-                        Text(text = "Camera")
+                Row {
+                    Button(onClick = {
+                        onOptionSelected(0)
+                        showDialog.value = false
+                    }) {
+                        Column {
+                            Image(
+                                painter = painterResource(R.drawable.add_photo_gallery),
+                                contentDescription = stringResource(R.string.gallery)
+                            )
+                            Text(text = stringResource(R.string.gallery))
+                        }
                     }
-                    TextButton(onClick = { onOptionSelected(1); showDialog.value = false }) {
-                        Text(text = "Gallery")
+                    Button(onClick = { /*TODO*/ }) {
+                        Column {
+                            Image(
+                                painter = painterResource(R.drawable.add_photo_camera),
+                                contentDescription = stringResource(R.string.camera)
+                            )
+                            Text(text = stringResource(R.string.camera))
+                        }
                     }
+
                 }
             }
         )
--- app/src/main/java/ncodedev/coffeebase/ui/utils/PermissionManager.kt
+++ app/src/main/java/ncodedev/coffeebase/ui/utils/PermissionManager.kt
@@ -1,28 +1,33 @@
 package ncodedev.coffeebase.ui.utils
-//
-//import android.Manifest
-//import android.os.Build
-//import androidx.activity.compose.ManagedActivityResultLauncher
-//import androidx.activity.compose.rememberLauncherForActivityResult
-//import androidx.activity.result.contract.ActivityResultContracts
-//import androidx.compose.runtime.Composable
-//import ncodedev.coffeebase.ui.screens.editcoffee.PermissionsViewModel
-//
-//
-//fun requestPermissions(viewModel: PermissionsViewModel): ManagedActivityResultLauncher<String, Boolean> {
-//    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) requestMediaPermission()
-//    else requestStoragePermission()
-//}
-//
-//private fun requestMediaPermission(viewModel: PermissionsViewModel): ManagedActivityResultLauncher<String, Boolean> {
-//    return rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission, onResult = )
-//    val storagePermissionResultLauncher = rememberLauncherForActivityResult(
-//        contract = ActivityResultContracts.RequestPermission(),
-//        onResult = { isGranted ->
-//            permissionsViewModel.onPermissionResult(
-//                permission = Manifest.permission.READ_MEDIA_IMAGES,
-//                isGranted = isGranted
-//            )
-//        }
-//    )
-//}
+
+import android.Manifest
+import androidx.activity.compose.ManagedActivityResultLauncher
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.runtime.Composable
+import ncodedev.coffeebase.ui.screens.editcoffee.PermissionsViewModel
+
+@Composable
+fun ReadMediaPermissionResultLauncher(permissionsViewModel: PermissionsViewModel): ManagedActivityResultLauncher<String, Boolean> {
+    return rememberLauncherForActivityResult(contract = ActivityResultContracts.RequestPermission(), onResult = { isGranted ->
+            permissionsViewModel.onPermissionResult(
+                permission = Manifest.permission.READ_MEDIA_IMAGES,
+                isGranted = isGranted
+            )
+            if (isGranted) {
+                showDialog.value = true
+            }
+    })
+}
+val readMediaPermissionResultLauncher = rememberLauncherForActivityResult(
+    contract = ActivityResultContracts.RequestPermission(),
+    onResult = { isGranted ->
+        permissionsViewModel.onPermissionResult(
+            permission = Manifest.permission.READ_MEDIA_IMAGES,
+            isGranted = isGranted
+        )
+        if (isGranted) {
+            showDialog.value = true
+        }
+    }
+)
